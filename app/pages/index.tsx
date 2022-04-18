fb308131c1852420ed90357fcdd16172915a9467
ellagoris <eduardofidera@gmail.com>
2022-04-18 04:27:41 -0300

    feat: move app

---
+++ app/pages/index.tsx
@@ -0,0 +1,265 @@
+/** @jsxImportSource theme-ui */
+import Head from "next/head"
+
+import { Button, Flex, Heading, Text } from "@theme-ui/components"
+
+import Header from "@/components/Header/Header"
+import NFTSelector from "@/components/NFTSelector/NFTSelector"
+import { PlusSign } from "@/components/icons"
+import { useAnchorWallet } from "@solana/wallet-adapter-react"
+import { useBreeding } from "@/hooks/useBreeding/useBreeding"
+import { web3 } from "@project-serum/anchor"
+import { LoadingIcon } from "@/components/icons/LoadingIcon"
+
+export default function Home() {
+  const anchorWallet = useAnchorWallet()
+  const {
+    initializeBreeding,
+    terminateBreeding,
+    initializeBreedingMachine,
+    breedingMachineAccount,
+    userBreedDatas,
+    feedbackStatus,
+  } = useBreeding()
+
+  console.log(breedingMachineAccount)
+  return (
+    <>
+      <Head>
+        <title>Breeding UI</title>
+        <meta name="description" content="Breeding UI created by MagicShards" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "4rem",
+        }}
+      >
+        <Heading mb=".8rem" variant="heading1">
+          Breed
+        </Heading>
+        <Text>Generate a new NFT from two!</Text>
+
+        <Button onClick={initializeBreedingMachine}>initialize</Button>
+
+        {breedingMachineAccount && (
+          <>
+            <hr />
+            <Text>
+              Burn parents:{" "}
+              {breedingMachineAccount.config.burnParents ? "Yes" : "No"}
+            </Text>
+            <Text>
+              Total breeding: {breedingMachineAccount.bred.toNumber()}
+            </Text>
+            <Text>Total born: {breedingMachineAccount.born.toNumber()}</Text>
+          </>
+        )}
+
+        <Flex
+          sx={{
+            alignItems: "center",
+            gap: ".8rem",
+            margin: "1.6rem 0",
+          }}
+        >
+          {feedbackStatus && (
+            <>
+              {feedbackStatus !== "Success!" && <LoadingIcon />}
+
+              <Text
+                sx={{
+                  color: feedbackStatus === "Success!" ? "success" : "text",
+                }}
+              >
+                {feedbackStatus}
+              </Text>
+            </>
+          )}{" "}
+          &nbsp;
+        </Flex>
+
+        <Flex
+          my="3.2rem"
+          sx={{
+            flexDirection: "column",
+            gap: "1.6rem",
+          }}
+        >
+          <Flex
+            sx={{
+              flexDirection: "column",
+              gap: ".8rem",
+              alignItems: "center",
+            }}
+          >
+            <Heading variant="heading3">Select two NFTs:</Heading>
+
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault()
+
+                const data = new FormData(e.currentTarget)
+                const mints = data.getAll("mint").filter((val) => val)
+
+                if (!anchorWallet?.publicKey) throw new Error("No public key.")
+
+                if (mints.length !== 2) return true
+
+                const res = await initializeBreeding(
+                  new web3.PublicKey(mints[0]),
+                  new web3.PublicKey(mints[1])
+                )
+
+                console.log(res)
+                return res
+              }}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "3.2rem",
+              }}
+            >
+              <Flex
+                sx={{
+                  gap: "1.6rem",
+                  flexDirection: "column",
+                  alignItems: "center",
+
+                  // "@media (min-width: 768px)": {
+                  //   flexDirection: "row",
+                  // },
+                }}
+              >
+                <NFTSelector name="mint" />
+
+                <PlusSign
+                  sx={{
+                    width: "3.2rem",
+                    height: "3.2rem",
+                    stroke: "text",
+                    strokeWidth: ".2rem",
+                  }}
+                />
+
+                <NFTSelector name="mint" />
+              </Flex>
+              <Button
+                sx={{
+                  alignSelf: "center",
+                }}
+                type="submit"
+              >
+                breed!
+              </Button>
+            </form>
+
+            <hr
+              sx={{
+                marginTop: "1.6rem",
+              }}
+            />
+            <Heading variant="heading2" mt="1.6rem">
+              Your breeds
+            </Heading>
+            {userBreedDatas ? (
+              userBreedDatas.length ? (
+                <Flex
+                  sx={{
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  {userBreedDatas.map((breedData) => (
+                    <Flex
+                      key={breedData.timestamp}
+                      sx={{
+                        flexDirection: "column",
+                        margin: "1.6rem 0",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Flex
+                        sx={{
+                          alignItems: "center",
+                          gap: ".8rem",
+                        }}
+                      >
+                        {breedData.mintA.toString().slice(0, 6) + "..."} +{" "}
+                        {breedData.mintB.toString().slice(0, 6) + "..."}
+                        <Button
+                          variant="secondary"
+                          onClick={() =>
+                            terminateBreeding(breedData.mintA, breedData.mintB)
+                          }
+                        >
+                          Terminate
+                        </Button>
+                      </Flex>
+                      <Flex>
+                        {new Date(
+                          breedData.timestamp.toNumber() * 1000
+                        ).toLocaleString()}
+                      </Flex>
+                    </Flex>
+                  ))}
+                </Flex>
+              ) : (
+                <Text>Empty.</Text>
+              )
+            ) : (
+              <LoadingIcon />
+            )}
+          </Flex>
+        </Flex>
+      </main>
+
+      <footer
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "4rem",
+        }}
+      >
+        <a
+          href="https://twitter.com/magicshards"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          Powered by{" "}
+          <Text
+            variant="small"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <img
+              sx={{
+                height: "32px",
+              }}
+              src="/magicshards.png"
+              alt="Magic Shards"
+              height={32}
+            />
+            MagicShards
+          </Text>
+        </a>
+      </footer>
+    </>
+  )
+}
---
+++ app/useBreeding/useBreeding.ts
@@ -0,0 +1,405 @@
+import { Provider, utils, web3, BN, Program, Idl } from "@project-serum/anchor"
+// @ts-ignore
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  Token,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token"
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react"
+import { useEffect, useState } from "react"
+import {
+  findBreedDataAddress,
+  findBreedingMachineAddress,
+  findWhitelistTokenAddress,
+} from "./utils"
+
+const candyMachineAddress = new web3.PublicKey(
+  "2foGcTHZ2C9c5xQrBopgLyNxQ33rdSxwDXqHJbv34Fvs"
+)
+
+const incineratorAddress = new web3.PublicKey(
+  "1nc1nerator11111111111111111111111111111111"
+)
+
+const feeToken = new web3.PublicKey(
+  "EmNtV2uNxC8kyGXSpLi8uLfgrmiTfEUhho4aqwUZdtMZ"
+)
+
+const programId = new web3.PublicKey(
+  "CikztTpnE9wiNzafzTCSzE4tXKFi5iHcGKzBhpNTiP7p"
+)
+
+const breedingMachine = findBreedingMachineAddress(
+  candyMachineAddress,
+  candyMachineAddress,
+  programId
+)
+
+const whitelistToken = findWhitelistTokenAddress(breedingMachine, programId)
+
+export const useBreeding = () => {
+  const { connection } = useConnection()
+  const anchorWallet = useAnchorWallet()
+  const [anchorProgram, setAnchorProgram] = useState<Program<Idl>>(null)
+  const [breedingMachineAccount, setBreedingMachineAccount] = useState(null)
+  const [userBreedDatas, setUserBreedDatas] = useState(null)
+  const [feedbackStatus, setFeedbackStatus] = useState("")
+
+  /**
+   * Fetch IDL, breeding machine and breed datas on mount
+   */
+  useEffect(() => {
+    const fetchData = async () => {
+      setFeedbackStatus("Fetching program...")
+      const provider = new Provider(connection, anchorWallet, {
+        preflightCommitment: "recent",
+      })
+
+      const idl = await Program.fetchIdl(programId, provider)
+
+      if (!idl)
+        throw new Error(
+          "No idl with address " +
+            programId.toString() +
+            " has been found on " +
+            process.env.NEXT_PUBLIC_CONNECTION_NETWORK +
+            "."
+        )
+
+      const anchorProgram = new Program(idl, programId, provider)
+
+      setAnchorProgram(anchorProgram)
+
+      try {
+        setFeedbackStatus("Fething breeding machine...")
+
+        const machineAccount = await anchorProgram.account.breedMachine.fetch(
+          breedingMachine
+        )
+
+        setBreedingMachineAccount(machineAccount)
+      } catch (e) {
+        console.error("Couldn't fetch breeding machine!" + e)
+      }
+
+      setFeedbackStatus("Fetching breeds...")
+      /** Fetch all program accounts */
+      const programAccs = await connection.getProgramAccounts(programId)
+
+      // try {
+      //   const multiple = await anchorProgram.account.breedData.fetchMultiple(
+      //     programAccs.map((programAcc) => programAcc.pubkey)
+      //   )
+
+      //   console.log(multiple)
+      // } catch (e) {
+      //   console.log(e)
+      // }
+
+      /** Get user breed datas */
+      const userBreedDatasPromises = programAccs.map(async (acc) => {
+        try {
+          /** Try to fetch breed data account using the program account address */
+          const breedData = await anchorProgram.account.breedData.fetch(
+            acc.pubkey
+          )
+
+          /** Return if the user wallet is the owner */
+          if (
+            breedData.owner.toString() === anchorWallet.publicKey.toString()
+          ) {
+            return breedData
+          }
+
+          return null
+        } catch (e) {
+          /** Error will be throwed if discriminator is invalid */
+          console.log(e)
+
+          return null
+        }
+      })
+
+      const breedDatas = (await Promise.all(userBreedDatasPromises)).filter(
+        (value) => value !== null
+      )
+
+      setUserBreedDatas(breedDatas)
+      setFeedbackStatus("")
+      console.log(breedDatas)
+    }
+
+    if (connection && anchorWallet?.publicKey) {
+      fetchData()
+    }
+  }, [connection, anchorWallet])
+
+  const initializeBreedingMachine = async () => {
+    const config = {
+      burnParents: false,
+      breedingTime: new BN(1),
+      rewardSupply: new BN(3333),
+      initializationFeeToken: feeToken,
+      initializationFeePrice: new BN(1),
+      rewardCandyMachine: candyMachineAddress,
+      parentsCandyMachine: candyMachineAddress,
+    }
+
+    const whitelistVault = await utils.token.associatedAddress({
+      mint: whitelistToken,
+      owner: breedingMachine,
+    })
+
+    const tx = await anchorProgram.methods
+      .createMachine(config)
+      .accounts({
+        breedingMachine,
+        whitelistToken,
+        whitelistVault,
+        authority: anchorWallet.publicKey,
+      })
+      .rpc()
+
+    console.log(tx)
+  }
+
+  const initializeBreeding = async (
+    mintParentA: web3.PublicKey,
+    mintParentB: web3.PublicKey
+  ) => {
+    try {
+      if (!anchorProgram) throw new Error("Anchor program is not initialized.")
+
+      if (!mintParentA || !mintParentB)
+        throw new Error("Mint addresses are missing.")
+
+      setFeedbackStatus("[Breed] Initializing...")
+
+      const breedData = findBreedDataAddress(
+        breedingMachine,
+        mintParentA,
+        mintParentB,
+        anchorProgram.programId
+      )
+
+      const userAtaParentA = await utils.token.associatedAddress({
+        mint: mintParentA,
+        owner: anchorWallet.publicKey,
+      })
+
+      const userAtaParentB = await utils.token.associatedAddress({
+        mint: mintParentB,
+        owner: anchorWallet.publicKey,
+      })
+
+      const vaultAtaParentA = await utils.token.associatedAddress({
+        mint: mintParentA,
+        owner: breedData,
+      })
+
+      const vaultAtaParentB = await utils.token.associatedAddress({
+        mint: mintParentB,
+        owner: breedData,
+      })
+
+      /**
+       * Additional instructions:
+       *
+       * Create ATA for fee payer if necessary
+       * Create ATA for incinerator if necessary
+       */
+      const additionalInstructions = []
+
+      const feePayerAtaAddress = await utils.token.associatedAddress({
+        mint: feeToken,
+        owner: anchorWallet.publicKey,
+      })
+
+      setFeedbackStatus("[Breed] Fetching accounts...")
+      const feePayerAtaAccountInfo = await connection.getAccountInfo(
+        feePayerAtaAddress
+      )
+
+      if (!feePayerAtaAccountInfo) {
+        const createAtaInstruction =
+          Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            feeToken,
+            feePayerAtaAddress,
+            anchorWallet.publicKey,
+            anchorWallet.publicKey
+          )
+
+        additionalInstructions.push(createAtaInstruction)
+      }
+
+      const feeIncineratorAtaAddress = await utils.token.associatedAddress({
+        mint: feeToken,
+        owner: incineratorAddress,
+      })
+
+      const feeIncineratorAtaAccountInfo = await connection.getAccountInfo(
+        feeIncineratorAtaAddress
+      )
+
+      if (!feeIncineratorAtaAccountInfo) {
+        const createAtaInstruction =
+          Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            feeToken,
+            feeIncineratorAtaAddress,
+            incineratorAddress,
+            anchorWallet.publicKey
+          )
+
+        additionalInstructions.push(createAtaInstruction)
+      }
+
+      const feePrice = new BN(1000000000)
+
+      setFeedbackStatus("[Breed] Sending transaction...")
+      const tx = await anchorProgram.methods
+        .initializeBreeding(feePrice)
+        .accounts({
+          breedingMachine,
+          breedData,
+
+          mintParentA,
+          userAtaParentA,
+          vaultAtaParentA,
+
+          mintParentB,
+          userAtaParentB,
+          vaultAtaParentB,
+
+          feeToken,
+          feePayerAta: feePayerAtaAddress,
+          feeIncineratorAta: feeIncineratorAtaAddress,
+
+          userWallet: anchorWallet.publicKey,
+        })
+        .preInstructions(additionalInstructions)
+        .signers([])
+        .rpc()
+
+      console.log(tx)
+
+      setFeedbackStatus("[Breed] Confirming transaction...")
+
+      await connection.confirmTransaction(tx, "confirmed")
+
+      setFeedbackStatus("Success!")
+
+      setTimeout(() => {
+        setFeedbackStatus("")
+      }, 6000)
+    } catch (e) {
+      console.log(e)
+
+      setFeedbackStatus("Something went wrong. " + e + "")
+    }
+  }
+
+  const terminateBreeding = async (
+    mintParentA: web3.PublicKey,
+    mintParentB: web3.PublicKey
+  ) => {
+    try {
+      if (!anchorProgram) throw new Error("Anchor program is not initialized.")
+
+      if (!mintParentA || !mintParentB)
+        throw new Error("Mint addresses are missing.")
+
+      setFeedbackStatus("[Breed] Terminating...")
+
+      const breedData = findBreedDataAddress(
+        breedingMachine,
+        mintParentA,
+        mintParentB,
+        anchorProgram.programId
+      )
+
+      const userAtaParentA = await utils.token.associatedAddress({
+        mint: mintParentA,
+        owner: anchorWallet.publicKey,
+      })
+
+      const userAtaParentB = await utils.token.associatedAddress({
+        mint: mintParentB,
+        owner: anchorWallet.publicKey,
+      })
+
+      const vaultAtaParentA = await utils.token.associatedAddress({
+        mint: mintParentA,
+        owner: breedData,
+      })
+
+      const vaultAtaParentB = await utils.token.associatedAddress({
+        mint: mintParentB,
+        owner: breedData,
+      })
+
+      const whitelistVault = await utils.token.associatedAddress({
+        mint: whitelistToken,
+        owner: breedingMachine,
+      })
+
+      const userWhitelistAta = await utils.token.associatedAddress({
+        mint: whitelistToken,
+        owner: anchorWallet.publicKey,
+      })
+
+      setFeedbackStatus("[Breed] Sending transaction...")
+      const tx = await anchorProgram.methods
+        .finalizeBreeding()
+        .accounts({
+          breedingMachine,
+          breedData,
+
+          mintParentA,
+          mintParentB,
+
+          userAtaParentA,
+          userAtaParentB,
+
+          vaultAtaParentA,
+          vaultAtaParentB,
+
+          whitelistToken,
+          whitelistVault,
+          userWhitelistAta,
+
+          userWallet: anchorWallet.publicKey,
+        })
+        .signers([])
+        .rpc()
+
+      console.log(tx)
+
+      setFeedbackStatus("[Breed] Confirming transaction...")
+
+      await connection.confirmTransaction(tx, "confirmed")
+
+      setFeedbackStatus("Success!")
+
+      setTimeout(() => {
+        setFeedbackStatus("")
+      }, 6000)
+    } catch (e) {
+      console.log(e)
+
+      setFeedbackStatus("Something went wrong. " + e + "")
+    }
+  }
+
+  return {
+    initializeBreeding,
+    terminateBreeding,
+    initializeBreedingMachine,
+    breedingMachineAccount,
+    userBreedDatas,
+    feedbackStatus,
+  }
+}
